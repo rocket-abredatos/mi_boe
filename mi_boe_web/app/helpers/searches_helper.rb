04b0779e7951866e48a5d735a11db839bca0cbef
Álvaro Martín Fraguas <amartin@paradigmatecnologico.com>
2011-05-07 19:43:21 +0200

    Finished views for search results and facets.

--- mi_boe_web/app/helpers/searches_helper.rb
+++ mi_boe_web/app/helpers/searches_helper.rb
@@ -1,2 +1,10 @@
 module SearchesHelper
+  def parse_facet_fields(category, category_i18n)
+    result = '<h3>' + t(category_i18n) + '</h3>'
+    attribute = @search.facet_counts.facet_fields.send(category)
+    for i in 0..(attribute.count / 2 - 1)
+      result += ' - <a>' + attribute[i*2].to_s + ' (' + attribute[i*2 + 1].to_s + ')</a><br/>'
+    end
+    result + '<br/><br/>'
+  end
 end
