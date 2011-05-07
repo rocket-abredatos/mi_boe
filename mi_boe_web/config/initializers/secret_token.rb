6fd681331f517116fed540db4fb9d03c41ae519e
Álvaro Martín Fraguas <amartin@paradigmatecnologico.com>
2011-05-07 10:37:01 +0200

    Created Ruby on Rails application.

---
+++ mi_boe_web/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiBoeWeb::Application.config.secret_token = '365f3f83d9e669af4a51a6225973d46a046ec4f738a44b0efa7ba642cf7280e76aa996e7a294492b31003c7534e07d857663b8b9e355fd0f74a1fdff2c746037'
---
+++ mi_boe_web/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MiBoeWeb::Application.config.session_store :cookie_store, :key => '_mi_boe_web_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MiBoeWeb::Application.config.session_store :active_record_store
