2df8ea128f267a351413df8f135c302e3315b752
Álvaro Martín Fraguas <amartin@paradigmatecnologico.com>
2011-05-08 18:01:22 +0200

    Improved pagination view.

--- mi_boe_web/app/helpers/searches_helper.rb
+++ mi_boe_web/app/helpers/searches_helper.rb
@@ -7,4 +7,41 @@ module SearchesHelper
     end
     result + '<br/><br/>'
   end
+
+  def pagination
+    result = ''
+    if @search.response.numFound > 0
+      if @search.response.numFound < 20
+        for i in 0..(@search.response.numFound / 10)
+          result += (page_link i) + ' | '
+        end
+      elsif (@search.response.start / 10) < 5
+        for i in 0..9
+          result += (page_link i) + ' | '
+        end
+        result += ' ... ' + (page_link @search.response.numFound / 10)
+      elsif (@search.response.start / 10) > (@search.response.numFound / 10 - 4) 
+        result += (page_link 0) + ' ... '
+        for i in (@search.response.numFound / 10 - 9)..(@search.response.numFound / 10)
+          result += (page_link i) + ' | '
+        end
+      else
+        result += (page_link 0) + ' ... '
+        for i in (@search.response.start / 10 - 4)..(@search.response.start / 10 + 4)
+          result += (page_link i) + ' | '
+        end
+        result += ' ... ' + (page_link @search.response.numFound / 10)
+      end
+    end
+    result
+  end
+  
+  private
+    def page_link(i)
+       if (i * 10) == @search.response.start
+        (i + 1).to_s
+      else
+       link_to((i + 1).to_s, {:action => "create", :search => {:query => @search.responseHeader.params.q, :start => (i * 10).to_s}}, {:method => 'post'})
+      end
+    end
 end
