088ca5fde3862954381be800cc076c66e58a39f9
Álvaro Martín Fraguas <amartin@paradigmatecnologico.com>
2011-05-08 17:23:40 +0200

    Added pagination.

--- mi_boe_web/app/controllers/searches_controller.rb
+++ mi_boe_web/app/controllers/searches_controller.rb
@@ -2,7 +2,15 @@ class SearchesController < ApplicationController
   # POST /searches
   # POST /searches.xml
   def create
-    query = SolrAPI::Query.find(:one, :from => SolrAPI::Query.prefix, :params => {:q => (params[:search][:query].present? ? params[:search][:query] : '*:*'), :wt => 'json'})
+    query = SolrAPI::Query.find(
+      :one,
+      :from => SolrAPI::Query.prefix,
+      :params => {
+        :q => (params[:search][:query].present? ? params[:search][:query] : '*:*'),
+        :start => (params[:search][:start].present? ? params[:search][:start] : '0'),
+        :wt => 'json'
+      }
+    )
     @search = Search.new(query.attributes)
 
     respond_to do |format|
