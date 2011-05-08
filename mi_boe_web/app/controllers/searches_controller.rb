f87e31e66865d39e4dd95aafb3a35bda060fd084
Álvaro Martín Fraguas <amartin@paradigmatecnologico.com>
2011-05-08 10:36:11 +0200

    Added handling of empty queries.

--- mi_boe_web/app/controllers/searches_controller.rb
+++ mi_boe_web/app/controllers/searches_controller.rb
@@ -40,7 +40,7 @@ class SearchesController < ApplicationController
   # POST /searches
   # POST /searches.xml
   def create
-    query = SolrAPI::Query.find(:one, :from => SolrAPI::Query.prefix, :params => {:q => params[:search][:query], :wt => 'json'})
+    query = SolrAPI::Query.find(:one, :from => SolrAPI::Query.prefix, :params => {:q => (params[:search][:query].present? ? params[:search][:query] : '*:*'), :wt => 'json'})
     @search = Search.new(query.attributes)
 
     respond_to do |format|
