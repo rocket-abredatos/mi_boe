57c885f51e810550d25e0dcc9955b0b6fb07554d
Álvaro Martín Fraguas <amartin@paradigmatecnologico.com>
2011-05-08 18:24:37 +0200

    Added facet query links.

--- mi_boe_web/app/controllers/searches_controller.rb
+++ mi_boe_web/app/controllers/searches_controller.rb
@@ -8,6 +8,7 @@ class SearchesController < ApplicationController
       :params => {
         :q => (params[:search][:query].present? ? params[:search][:query] : '*:*'),
         :start => (params[:search][:start].present? ? params[:search][:start] : '0'),
+        :fq => (params[:search][:fq].present? ? (params[:search][:fq].split(':')[0] + ":\"" + params[:search][:fq].split(':')[1] + "\"") : ''),
         :wt => 'json'
       }
     )
--- mi_boe_web/app/helpers/searches_helper.rb
+++ mi_boe_web/app/helpers/searches_helper.rb
@@ -3,7 +3,7 @@ module SearchesHelper
     result = '<h3>' + t(category_i18n) + '</h3>'
     attribute = @search.facet_counts.facet_fields.send(category)
     for i in 0..(attribute.count / 2 - 1)
-      result += ' - <a>' + attribute[i*2].to_s + ' (' + attribute[i*2 + 1].to_s + ')</a><br/>'
+      result += '<br/> - ' + link_to(attribute[i*2].to_s + ' (' + attribute[i*2 + 1].to_s + ')', {:action => "create", :search => {:query => @search.responseHeader.params.q, :fq => category + ':' + attribute[i*2].to_s}}, {:method => 'post'})
     end
     result + '<br/><br/>'
   end
